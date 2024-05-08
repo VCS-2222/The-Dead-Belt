5b9893b7ab7055f2d1af0d4ef291d8741762b9c3
VCS-2222 <520946@chrome.escg.ac.uk>
2024-05-08 23:57:32 +0100

    Cave Décor

--- Assets/Scripts/Zombies/ZombieAnimatorController.cs
+++ Assets/Scripts/Zombies/ZombieAnimatorController.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieAnimatorController : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] Animator animator;
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("Values")]
+    [SerializeField] float oneDimentionAnimatorBlend;
+
+    private void Start()
+    {
+        oneDimentionAnimatorBlend = 0;
+    }
+
+    private void Update()
+    {
+        SetBlendToVelotity();
+    }
+
+    public void SetBlendToVelotity()
+    {
+        oneDimentionAnimatorBlend = agent.velocity.magnitude;
+        animator.SetFloat("blend", oneDimentionAnimatorBlend);
+    }
 }
